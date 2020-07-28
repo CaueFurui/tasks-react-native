64faca3a3adbe8350468edfbc189ccb629890ff3
CaueFurui <cauefurui@gmail.com>
2020-07-27 23:13:18 -0300

    [DRAWER]Styled the drawer

--- src/screens/Menu/Menu.js
+++ src/screens/Menu/Menu.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
-import { Text, View, ScrollView } from 'react-native'
-import {DrawerItems} from 'react-navigation-drawer'
+import React from 'react'
+import { Text, View, ScrollView, Platform } from 'react-native'
+import { DrawerItems } from 'react-navigation-drawer'
 import styles from './styles'
+import { Gravatar } from 'react-native-gravatar'
 
 export default props => {
+
+    const optionsGravatar = {
+        email: props.navigation.getParam('email'),
+        secure: true
+    }
+
     return (
         <ScrollView>
+            <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
+                <Gravatar style={styles.avatar}
+                    options={optionsGravatar}/>
+                <View style={styles.userInfo}>
+                    <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
+                    <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
+                </View>
+            </View>
             <DrawerItems {...props}/>
         </ScrollView>
     )
--- src/screens/Menu/styles.js
+++ src/screens/Menu/styles.js
@@ -1,7 +1,41 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
+import global from '../../global'
 
 const styles = StyleSheet.create({
-    
+    header: {
+        borderBottomWidth: 1,
+        borderColor: '#ddd'
+    },
+    avatar: {
+        width: 60,
+        height: 60,
+        borderWidth: 3,
+        borderRadius: 30,
+        margin: 10,
+        backgroundColor: '#222',
+    },
+    title: {
+        color: '#000',
+        fontFamily: global.fontFamily,
+        fontSize: 30,
+        padding: 10,
+        marginTop: Platform.OS === 'ios' ? 70 : 30
+    },
+    userInfo: {
+        marginLeft: 10
+    },
+    name: {
+        fontFamily: global.fontFamily,
+        fontSize: 20,
+        marginBottom: 5,
+        color: global.colors.mainText
+    },
+    email: {
+        fontFamily: global.fontFamily,
+        fontSize: 15,
+        color: global.colors.subText,
+        marginBottom: 10
+    }
 })
 
 export default styles
