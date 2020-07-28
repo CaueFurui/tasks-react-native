5523f8353bd215be04fe9bcd80592561af40cf12
CaueFurui <cauefurui@gmail.com>
2020-07-27 23:42:27 -0300

    [ERROR]Error handling

--- src/common.js
+++ src/common.js
@@ -4,7 +4,8 @@ const server = Platform.OS === 'ios'
     ? 'http://localhost:3000' : 'http://192.168.0.14:3000'
 
 function showError(err) {
-    Alert.alert('Opss! Ocorreu um problema!', `Mensagem: ${err}`)
+    if(err.response && err.response.data)
+    Alert.alert('Opss! Ocorreu um problema!', `Mensagem: ${err.response.data}`)
 }
 
 function showSuccess(msg) {
