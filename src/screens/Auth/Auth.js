723bf63e28c45b8d07d4fb46d48cd1659c7dc6ef
CaueFurui <cauefurui@gmail.com>
2020-07-27 21:41:54 -0300

    [REGISTER]Integrated register function with the API

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -3,30 +3,52 @@ import {
     Text,
     View,
     ImageBackground,
-    TextInput,
     TouchableOpacity,
     Alert } from 'react-native'
 import styles from './styles'
 import backgroundImage from '../../../assets/imgs/login.jpg'
 import AuthInput from '../../components/AuthInput'
+import { server, showError, showSuccess } from '../../common'
+import axios from 'axios'
+
+initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false
+}
 
 export default class Auth extends Component {
 
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        stageNew: false
+        ...initialState
     }
 
     signInOrSignUp = () => {
         if(this.state.stageNew) {
-            Alert.alert('Sucesso!', 'Criar a conta')
+            this.signup()
         } else {
             Alert.alert('Sucesso!', 'Logar')
         }
     }
+
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword,
+            })
+
+            showSuccess('Usuário cadastro!')
+            this.setState({ ...initialState })
+        } catch(e) {
+            showError(e)
+        }
+    }
+
 
     render() {
         return (
